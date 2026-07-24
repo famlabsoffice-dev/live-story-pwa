e58133142fe1bd56da299a6bef0ede2fcf502076
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-07-24 17:34:01 +0200

    feat(database): add database exports

---
+++ src/database/index.ts
@@ -0,0 +1,15 @@
+export { db, LiveStoryDatabase } from "./db";
+
+export type {
+  BaseEntity,
+  Chapter,
+  Interview,
+  Media,
+  Memory,
+  Person,
+  Place,
+  Relation,
+  Story,
+  SyncQueueItem,
+  Tag,
+} from "./schema";
