3bc82782fbc183e302dbd85e8f9912755df52230
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-07-24 22:43:51 +0200

    feat(module7): export personal story reasoning layer

--- src/domain/memoryGraph/index.ts
+++ src/domain/memoryGraph/index.ts
@@ -4,3 +4,4 @@ export * from "./memoryGraphStorage";
 export * from "./privacyBoundary";
 export * from "./memoryRetrievalEngine";
 export * from "./relationshipIntelligence";
+export * from "./storyReasoningLayer";
